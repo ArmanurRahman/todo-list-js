a2b9c50208aa3c9ab04f3b0209fdb0ade7e59dd9
Armanur Rahman <mubeenarman19@gmail.com>
2021-09-23 12:47:17 +0900

    add filter on todo list

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
 
 const addTodo = (event) => {
     event.preventDefault();
@@ -54,6 +55,33 @@ const deleteCheck = (event) => {
     }
 };
 
+const filterTodo = (e) => {
+    const todos = todoList.childNodes;
+
+    todos.forEach((todo) => {
+        switch (e.target.value) {
+            case "all":
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                if (todo.classList.contains("completed")) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+            case "uncompleted":
+                if (!todo.classList.contains("completed")) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+        }
+    });
+};
+
 //add event  listener
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("change", filterTodo);
